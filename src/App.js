22c7b2ffafd2f9950aa2dc133e9bf89a5a62cab0
ValeriiaBond <valeriaredkina19@gmail.com>
2021-03-03 17:47:57 +0200

    Changes in App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import LoginForm from './components/LoginForm'
 function App() {
   const adminUser = {
     email: "admin@admin.com",
-    password: "admin123" 
+    password: "admin123"
   }
 
-  const [user, setUser] = useState({name: "", email: ""});
+  const [user, setUser] = useState({ name: "", email: "" });
   const [error, setError] = useState("");
 
   const Login = details => {
@@ -23,23 +23,23 @@ function App() {
       console.log("Details do not match");
       setError("Details do not match");
     }
-  } 
+  }
 
   const Logout = () => {
-    setUser({name: "", email: ""});
+    setUser({ name: "", email: "" });
   }
 
 
   return (
     <div className="App">
       {(user.email != "") ? (
-        <div className = "welcome">
+        <div className="welcome">
           <h2>Welcome <span>{user.name}</span></h2>
           <button onClick={Logout}>Logout</button>
         </div>
       ) : (
-        <LoginForm Login={Login} error={error}/>
-      )}
+          <LoginForm Login={Login} error={error} />
+        )}
     </div>
   );
 }
